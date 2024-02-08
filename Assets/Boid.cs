fd7bc27992f35cfc75510c628540a579f9081a76
Michael Barrows <michael.barrows.a@gmail.com>
2024-02-07 22:04:28 -0500

    added IsNearEdge() check

--- Assets/Boid.cs
+++ Assets/Boid.cs
@@ -8,20 +8,40 @@ public class Boid : MonoBehaviour
 {
     public Rigidbody2D rb;
     public int max_speed = 10;
-
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.transform.Rotate(rb.transform.forward, UnityEngine.Random.Range(1, 359));
+        transform.Rotate(transform.forward, UnityEngine.Random.Range(1, 359));
     }
     private void FixedUpdate()
     {
-        rb.velocity = rb.transform.up * max_speed * Time.deltaTime;
+        rb.velocity = transform.up * max_speed * Time.deltaTime;
+    }
+    private (float,float) GetCamBounds()
+    {
+        Camera cam = Camera.main;
+        float height = 2f * cam.orthographicSize;
+        float width = height * cam.aspect;
+        (float widthAbs, float heightAbs) CamBounds = (width/2, height/2);
+        return CamBounds;
+    }
+    private bool IsNearEdge()
+    {
+        (float widthAbs,float heightAbs) CamBounds = GetCamBounds();
+
+        if ( Mathf.Abs(transform.position.x) > CamBounds.widthAbs*0.9f || Mathf.Abs(transform.position.y) > CamBounds.heightAbs * 0.9f)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
     // Update is called once per frame
     void Update()
     {
-        
+        IsNearEdge();
     }
 }
